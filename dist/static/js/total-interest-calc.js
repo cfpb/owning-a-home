c81da4055842c848795897cf409f51b1d965d08e
Adam Scott <adam.scott@cfpb.gov>
2014-04-11 14:30:49 -0400

    move files to module

--- dist/static/js/total-interest-calc.js
+++
@@ -1,8 +0,0 @@
-var Financial = require('financial');
-var MakeMoney = require('./make-money.js');
-
-// calculate the total interest paid on a loan
-module.exports = function(loanRate, termLength, loanAmt, startPeriod, endPeriod, type) {
-  var totalInterest = Financial.CUMIPMT(loanRate / 100 / 12, termLength * 12, loanAmt, startPeriod, endPeriod * 12, type);
-  return MakeMoney(totalInterest);
-};
