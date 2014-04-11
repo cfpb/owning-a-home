aa55314684634b16e7612e8d5fe9a9787abddd4b
Adam Scott <adam.scott@cfpb.gov>
2014-04-11 14:30:49 -0400

    move files to module

--- dist/static/js/payment-calc.js
+++
@@ -1,8 +0,0 @@
-var Financial = require('financial');
-var MakeMoney = require('./make-money.js');
-
-// calculate the amount of a monthly payment
-module.exports = function(loanRate, termLength, loanAmt) {
-  var monthlyPayment = Financial.PMT(loanRate / 100 / 12, termLength * 12, loanAmt);
-  return MakeMoney(monthlyPayment);
-};
