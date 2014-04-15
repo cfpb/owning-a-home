6b159c6dab42aff0f17d81aaa0bb56b4e820bab1
Adam Scott <adam.scott@cfpb.gov>
2014-04-15 09:19:00 -0400

    initial mocha test setup

---
+++ test/format-usd_test.js
@@ -0,0 +1,16 @@
+var formatUSD = require('../src/static/js/modules/format-usd.js');
+
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('Formats USD', function() {
+
+  it('formats a number to USD format', function() {
+    expect(formatUSD(200000)).to.equal('$200,000.00');
+  });
+
+  it('formats a numerical string to USD format', function() {
+    expect(formatUSD('200000')).to.equal('$200,000.00');
+  });
+
+});
