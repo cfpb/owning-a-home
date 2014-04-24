c077c72af2eae613470c4de13cb2f0673fa8e0a9
Adam Scott <adam.scott@cfpb.gov>
2014-04-24 06:06:11 -0400

    point to font files in ip folder

--- dist/static/js/main.js
+++ dist/static/js/main.js
@@ -12123,7 +12123,6 @@ var formatUSD = require('./modules/format-usd');
 var unFormatUSD = require('./modules/unformat-usd');
 var highcharts = require('highcharts');
 
-console.log('TEST');
 // This is a temporary function that generates fake data in
 // the same format that our API will eventually return it.
 var mock = function() {
