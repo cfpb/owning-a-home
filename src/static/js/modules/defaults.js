5c357c64c49a271cb2e0f142e752f508b6c78d3f
Chris Contolini <christopher.contolini@cfpb.gov>
2014-04-29 14:01:57 -0400

    Store user's location in local storage

--- src/static/js/modules/defaults.js
+++ src/static/js/modules/defaults.js
@@ -1,19 +1,33 @@
 // Intelligent defaults
 var getState = require('./geolocation');
 
+var defaults = {
+  state: localStorage.getItem( 'state' )
+};
+
 var loadDefaults = function( cb ) {
+
+  if ( defaults.state ) {
+    setState();
+    return cb();
+  }
 
   // Get their state using the HTML5 gelocation API.
   navigator.geolocation.getCurrentPosition( loadState, noLocation );
 
   function loadState( pos ){
-    var state = getState( pos );
-    $('#location').val( state );
+    defaults.state = getState( pos );
+    localStorage.setItem( 'state', defaults.state );
+    setState();
     cb();
   }
 
   function noLocation() {
     cb();
+  }
+
+  function setState() {
+    $('#location').val( defaults.state );
   }
 
 };
