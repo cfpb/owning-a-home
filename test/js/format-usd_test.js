4869ed2d8541cb750c336aa7493b158cdec64530
Orlando Soto <Orlando.Soto-Pastor@cfpb.gov>
2014-05-14 09:36:13 -0400

    Added negative tests for formatting US currency

--- test/js/format-usd_test.js
+++ test/js/format-usd_test.js
@@ -9,8 +9,36 @@ describe('Formats USD', function() {
     expect(formatUSD(200000)).to.equal('$200,000.00');
   });
 
+  it('Negative test - this should fail based on the number of decimal places passed as argument', function() {
+    expect(formatUSD(200000, 5)).to.equal('$200,000.00');
+  });
+
+  it('Negative test - passing a negative number', function() {
+    expect(formatUSD(-200000)).to.equal('-$200,000.00');
+  });
+
+  it('Negative test - passing a decimal number', function() {
+    expect(formatUSD(-200000.1)).to.equal('-$200,000.10');
+  });
+
   it('formats a numerical string to USD format', function() {
     expect(formatUSD('200000')).to.equal('$200,000.00');
+  });
+
+  it('Negative test - this should fail based on the number of decimal places passed as argument', function() {
+    expect(formatUSD('200000', 5)).to.equal('$200,000.00');
+  });
+
+  it('Negative test - passing a negative numerical string', function() {
+    expect(formatUSD('-200000')).to.equal('-$200,000.00');
+  });
+
+  it('Negative test - passing a character string', function() {
+    expect(formatUSD('ABC')).to.equal('$0.00');
+  });
+
+  it('Negative test - passing a string with invalid characters', function() {
+    expect(formatUSD('@/#$%^&*.!')).to.equal('$0.00');
   });
 
 });
