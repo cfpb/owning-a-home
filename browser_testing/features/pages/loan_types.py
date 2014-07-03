cf52c3f6e596fdbf299901e11e203f2afe3413ff
Orlando Soto <Orlando.Soto-Pastor@cfpb.gov>
2014-07-03 14:56:19 -0400

    formatting changes and switched to master branch

--- browser_testing/features/pages/loan_types.py
+++ browser_testing/features/pages/loan_types.py
@@ -1,9 +1,13 @@
 
 from selenium.common.exceptions import NoSuchElementException
-from pages.base import *
 from selenium import webdriver
 
-class LoanTypes(Base):      
+from pages.base import Base
 
-    def __init__(self, logger, base_url=r'http://localhost/', driver=None, driver_wait=-1, delay_secs=0):
-        super(LoanTypes, self).__init__(logger, base_url, driver, driver_wait, delay_secs)
+
+class LoanTypes(Base):
+
+    def __init__(self, logger, directory, base_url=r'http://localhost/',
+                 driver=None, driver_wait=-1, delay_secs=0):
+        super(LoanTypes, self).__init__(logger, directory, base_url,
+                                        driver, driver_wait, delay_secs)
