b2f882ef0c7f59d7b0e62ffe9966238a5fabba50
Chris Contolini <christopher.contolini@cfpb.gov>
2014-08-04 13:09:13 -0400

    Introduce objectify

--- src/js/modules/loan-comparison.js
+++ src/js/modules/loan-comparison.js
@@ -1,40 +1,49 @@
 var debounce = require('debounce');
 var cost = require('overall-loan-cost');
+var objectify = require('cf-objectify');
 var formatUSD = require('format-usd');
 var unFormatUSD = require('unformat-usd');
 var isMoney = require('is-money-usd');
 var amortize = require('amortize');
 
+var loan = objectify([
+  {
+    name: 'location',
+    source: 'location'
+  },{
+    name: 'credit-score',
+    source: 'credit-score'
+  },{
+    name: 'amount-borrowed',
+    source: 'house-price - down-payment'
+  },{
+    name: 'rate-structure',
+    source: 'rate-structure'
+  },{
+    name: 'loan-term',
+    source: 'loan-term'
+  },{
+    name: 'loan-type',
+    source: 'loan-type'
+  },{
+    name: 'arm-type',
+    source: 'arm-type'
+  },{
+    name: 'interest-rate',
+    source: 'interest-rate'
+  }
+]);
+
 function updateComparisons() {
 
   var $monthly = $('#monthly-payment span'),
       $overall = $('#overall-costs span'),
-      loanInfo = getLoanInfo(),
-      costs = calcCosts( loanInfo );
+      costs = calcCosts();
 
   $monthly.text( formatUSD(costs.monthlyPayment) );
   $overall.text( formatUSD(costs.overallCost) );
 
-}
-
-function getLoanInfo() {
-
-  function _sanitizeMoney( val ) {
-    return isMoney( val ) ? unFormatUSD( val ) : 0;
-  }
-
-  var vals = {
-    location: $('#location').val(),
-    creditScore: $('#credit-score-select').val(),
-    amountBorrowed: _sanitizeMoney( $('#house-price-input').val() - $('#down-payment-input').val() ),
-    rateStructure: $('#rate-structure-select').val(),
-    termLength: $('#loan-term-select').val(),
-    loanType: $('#loan-type-select').val(),
-    armType: $('#arm-type-select').val(),
-    rate: $('#interest-rate-select').val()
-  };
-
-  return vals;
+  console.log(loan);
 
 }
 
@@ -42,32 +51,29 @@ function getLoanInfo() {
  * Calculate loan costs given loan details provided by the user.
  * @return {object} monthly payment and overall cost of the loan
  */
-function calcCosts( opts ) {
+function calcCosts() {
 
-  var loan,
+  var amortized,
       overall;
 
-  opts = opts || {};
-
-  loan = amortize({
-    amount: opts.amountBorrowed,
-    rate: opts.rate,
-    totalTerm: opts.termLength,
+  amortized = amortize({
+    amount: loan['amount-borrowed'],
+    rate: loan['interest-rate'],
+    totalTerm: loan['loan-term'],
     amortizeTerm: 60
   });
 
   overall = cost({
-    amountBorrowed: opts.amountBorrowed,
-    rate: opts.rate,
-    totalTerm: opts.termLength,
+    amountBorrowed: loan['amount-borrowed'],
+    rate: loan['interest-rate'],
+    totalTerm: loan['loan-term'],
   });
 
   return {
-    monthlyPayment: loan.payment,
+    monthlyPayment: amortized.payment,
     overallCosts: overall.overallCost
   };
 
 }
 
 $('.comparisons').on( 'change keyup', '.recalc', debounce(updateComparisons, 500) );
-
