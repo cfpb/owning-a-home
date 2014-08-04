6f5c3077595051ea1d3fc3c1805eefca5af8b654
Chris Contolini <christopher.contolini@cfpb.gov>
2014-08-04 15:53:18 -0400

    Move logic into objectify constructor

--- src/js/modules/loan-comparison.js
+++ src/js/modules/loan-comparison.js
@@ -5,14 +5,18 @@ var formatUSD = require('format-usd');
 var unFormatUSD = require('unformat-usd');
 var isMoney = require('is-money-usd');
 var amortize = require('amortize');
+require('./object.observe-polyfill');
 
 var loan = objectify([
   {
     name: 'location',
     source: 'location'
   },{
-    name: 'credit-score',
+    name: 'minfico',
     source: 'credit-score'
+  },{
+    name: 'maxfico',
+    source: 'credit-score + 20'
   },{
     name: 'amount-borrowed',
     source: 'house-price - down-payment'
@@ -31,48 +35,39 @@ var loan = objectify([
   },{
     name: 'interest-rate',
     source: 'interest-rate'
+  },{
+    name: 'monthly-payment',
+    source: function() {
+      var amortized = amortize({
+        amount: loan['amount-borrowed'],
+        rate: loan['interest-rate'],
+        totalTerm: loan['loan-term'],
+        amortizeTerm: 60
+      });
+      return amortized.payment;
+    }
+  },{
+    name: 'overall-cost',
+    source: function() {
+      var overall = cost({
+        amountBorrowed: loan['amount-borrowed'],
+        rate: loan['interest-rate'],
+        totalTerm: loan['loan-term'],
+      });
+      return overall.overallCost;
+    }
   }
 ]);
 
 function updateComparisons() {
 
   var $monthly = $('#monthly-payment span'),
-      $overall = $('#overall-costs span'),
-      costs = calcCosts();
+      $overall = $('#overall-costs span');
 
-  $monthly.text( formatUSD(costs.monthlyPayment) );
-  $overall.text( formatUSD(costs.overallCost) );
+  $monthly.text( formatUSD(loan['monthly-payment']) );
+  $overall.text( formatUSD(loan['overall-cost']) );
 
   console.log(loan);
-
-}
-
-/**
- * Calculate loan costs given loan details provided by the user.
- * @return {object} monthly payment and overall cost of the loan
- */
-function calcCosts() {
-
-  var amortized,
-      overall;
-
-  amortized = amortize({
-    amount: loan['amount-borrowed'],
-    rate: loan['interest-rate'],
-    totalTerm: loan['loan-term'],
-    amortizeTerm: 60
-  });
-
-  overall = cost({
-    amountBorrowed: loan['amount-borrowed'],
-    rate: loan['interest-rate'],
-    totalTerm: loan['loan-term'],
-  });
-
-  return {
-    monthlyPayment: amortized.payment,
-    overallCosts: overall.overallCost
-  };
 
 }
 
