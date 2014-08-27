30aa13576f063cd60c71dff14b19401a670b27b2
Chris Contolini <christopher.contolini@cfpb.gov>
2014-08-27 16:49:41 -0400

    Create less-than-ie8 file

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,10 +53,10 @@ module.exports = function(grunt) {
       },
       ie9: {
         src: ['src/js/legacy/lte-ie9.js', 'node_modules/es5-shim/es5-shim.js', 'src/vendor/polyfill/web.js', 'src/vendor/Placeholders.js/lib/utils.js', 'src/vendor/Placeholders.js/lib/main.js'],
-        dest: 'static/js/ie9.js',
+        dest: 'static/js/lte-ie9.js',
       },
       ie8: {
-        src: ['src/js/legacy/lte-ie9.js', 'node_modules/es5-shim/es5-shim.js', 'src/vendor/Placeholders.js/lib/utils.js', 'src/vendor/Placeholders.js/lib/main.js'],
+        src: ['src/js/legacy/lte-ie8.js'],
         dest: 'static/js/lte-ie8.js',
       }
     },
--- src/js/legacy/lte-ie9.js
+++ src/js/legacy/lte-ie9.js
@@ -2,25 +2,3 @@ if ( !console ) {
   console = {};
   console.log = function(){};
 }
-
-if (typeof Object.create != 'function') {
-    (function () {
-        var F = function () {};
-        Object.create = function (o) {
-            if (arguments.length > 1) {
-              throw Error('Second argument not supported');
-            }
-            if (typeof o != 'object') {
-              throw TypeError('Argument must be an object');
-            }
-            F.prototype = o;
-            return new F();
-        };
-    })();
-}
-
-if (!Object.isFrozen) {
-    Object.isFrozen = function isFrozen(object) {
-        return false;
-    };
-}
