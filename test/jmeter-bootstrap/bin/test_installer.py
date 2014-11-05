e313230407944d190905fae0e9972bc18ae5386b
Orlando Soto <Orlando.Soto-Pastor@cfpb.gov>
2014-11-05 12:42:25 -0500

    added jmeter-bootstrap to intall jmeter and dependencies

---
+++ test/jmeter-bootstrap/bin/test_installer.py
@@ -0,0 +1,11 @@
+import unittest, JMeterInstaller
+
+class JMeterInstallerTests(unittest.TestCase):
+
+    def test_install(self):
+        jmi = JMeterInstaller.JMeterInstaller()
+        res = jmi.install()
+        self.assertTrue(res)
+
+if __name__ == '__main__':
+    unittest.main()
