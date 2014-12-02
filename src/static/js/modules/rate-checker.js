23b3178d7ebd627a609b5925c5f32c1e678396db
Chuck Werner <charles.werner@cfpb.gov>
2014-12-02 10:55:54 -0500

    High-balance fixes

--- src/static/js/modules/rate-checker.js
+++ src/static/js/modules/rate-checker.js
@@ -326,7 +326,7 @@ function renderLoanAmount() {
  * @return {null}
  */
 function checkForJumbo() {
-
+  params.update();
   var loan,
       jumbos = ['jumbo', 'agency', 'fha-hb', 'va-hb'],
       request;
@@ -347,7 +347,8 @@ function checkForJumbo() {
   // Otherwise, make sure the county dropdown is shown.
   dropdown('county').show();
 
-  // Show a message if appropriate.
+  // Hide any existing message, then show a message if appropriate.
+  $('#county-warning').addClass('hidden');
   if ( params['loan-type'] === 'conf' ) {
     $('#county-warning').removeClass('hidden').find('p').text( template.countyConfWarning );
   }
@@ -407,10 +408,11 @@ function loadCounties() {
 }
 
 function processCounties() {
-
+  params.update();
   var $counties = $('#county'),
       $county = $('#county').find(':selected'),
       $loan = dropdown('loan-type'),
+      loanType = $('#loan-type').val(),
       norms = ['conf', 'fha', 'va'],
       jumbos = ['jumbo', 'agency', 'fha-hb', 'va-hb'],
       loan;
@@ -427,7 +429,6 @@ function processCounties() {
     fhaCountyLimit: parseInt( $county.data('fha'), 10 ),
     vaCountyLimit: parseInt( $county.data('va'), 10 )
   });
-
   if ( loan.success && loan.isJumbo ) {
     switch ( loan.type ) {
       case 'agency':
@@ -459,7 +460,8 @@ function processCounties() {
         });
         break;
     }
-    dropdown('loan-type').disable( norms );
+    dropdown('loan-type').enable( norms );
+    dropdown('loan-type').disable( loanType );
     dropdown('loan-type').showHighlight();
     $('#hb-warning').removeClass('hidden').find('p').text( loan.msg );
   } else {
@@ -942,7 +944,7 @@ function setSelections( options ) {
 // Recalculate everything when drop-down menus are changed.
 $('.demographics, .calc-loan-details').on( 'change', '.recalc', function() {
   checkForJumbo();
-  updateView();
+  processCounties();
 });
 
 // Prevent non-numeric characters from being entered
