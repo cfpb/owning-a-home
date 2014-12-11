97ce200d82600dcfcc9f5dbf4b745120a76d184c
Adam Scott <adam.scott@cfpb.gov>
2014-12-11 10:51:08 -0500

    add isArray polyfill

--- src/static/js/legacy/lte-ie8.js
+++ src/static/js/legacy/lte-ie8.js
@@ -197,3 +197,10 @@ if (!Array.prototype.indexOf) {
     return -1;
   };
 }
+
+//polyfill isArray
+if (!Array.isArray) {
+  Array.isArray = function(arg) {
+    return Object.prototype.toString.call(arg) === '[object Array]';
+  };
+}
