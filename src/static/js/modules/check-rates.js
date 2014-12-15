dc89545cdee0115f9f9c8acf3c3dfe30c5b2d478
Chuck Werner <charles.werner@cfpb.gov>
2014-12-15 16:15:20 -0500

    Removed debounce in favor of simpler delay

--- src/static/js/modules/check-rates.js
+++ src/static/js/modules/check-rates.js
@@ -107,6 +107,19 @@ var options = {
   'request': ''
 };
 
+/** 
+ * Simple (anonymous) delay function 
+ * @return {object} function that has been delayed
+ */
+var delay = (function(){ 
+  var t = 0;
+  return function(callback, delay) {
+    clearTimeout(t);
+    t = setTimeout(callback, delay);
+  };
+})();
+
+
 /**
  * Get data from the API.
  * @return {object} jQuery promise.
@@ -1102,14 +1115,18 @@ $('.calc-loan-amt .recalc').on( 'keyup', function(ev) {
   }
 });
 
-// debounced function for processing and updating
-$('.calc-loan-amt .recalc').on( 'keyup', debounce(
-  function(ev) {
-    // Don't recalculate on TAB or arrow keys
-    if ( ev.which !== 9 && ( ev.which < 37 || ev.which > 40 ) ) {
-      processLoanAmount( this );
-    }
-  }, 500, false));
+// delayed function for processing and updating
+$('.calc-loan-amt').on( 'keyup', '.recalc', function(ev) {
+  var verbotenKeys = [ 9, 37, 38, 39, 40 ],
+      element = this;
+
+  // Don't recalculate on TAB or arrow keys
+  if ( verbotenKeys.indexOf( ev.which ) === -1 ) {
+    delay(function() {
+      processLoanAmount( element );
+    }, 500);
+  }  
+});
 
 // Once the user has edited fields, put the kibosh on the placeholders
 $('#house-price, #percent-down, #down-payment').on( 'keyup', function( ev ) {
