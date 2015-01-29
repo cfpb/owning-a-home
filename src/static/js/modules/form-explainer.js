963b67e6e3ee03cc10be687fcb25060476655126
Mike Morici <Michael.Morici@cfpb.gov>
2015-01-29 14:25:49 -0500

    Better sticky form, it stops sticking to top when scrolling to bottom.

--- src/static/js/modules/form-explainer.js
+++ src/static/js/modules/form-explainer.js
@@ -12,28 +12,6 @@ var fx = {
   $window:          $( window )
 };
 
-// http://stackoverflow.com/questions/1682495/jquery-resize-to-aspect-ratio
-$.fn.resizeHeightMaintainRatio = function( newHeight ) {
-  var aspectRatio = $( this ).data('aspectRatio');
-  if ( aspectRatio === undefined ) {
-    aspectRatio = $( this ).width() / $( this ).height();
-    $( this ).data( 'aspectRatio', aspectRatio );
-  }
-  $( this ).height( newHeight );
-  $( this ).width( parseInt(newHeight * aspectRatio, 10) );
-};
-
-/**
- * Initialize the form explainer app.
- * @return {null}
- */
-function init() {
-  $(document).ready(function(){
-    setDimensions();
-    fx.$imageMapWrapper.sticky({ topSpacing: 0 });
-  });
-}
-
 function setDimensions() {
   var windowHeight = fx.$window.height(),
       wrapperWidth = fx.$wrapper.width(),
@@ -56,11 +34,43 @@ function setDimensions() {
   }
 }
 
-// ...
-// $('.form-expainer').click(function(ev){
-//   ev.preventDefault();
-//   console.log('you clicked',ev.target);
-// });
+// http://stackoverflow.com/questions/1682495/jquery-resize-to-aspect-ratio
+$.fn.resizeHeightMaintainRatio = function( newHeight ) {
+  var aspectRatio = $( this ).data('aspectRatio');
+  if ( aspectRatio === undefined ) {
+    aspectRatio = $( this ).width() / $( this ).height();
+    $( this ).data( 'aspectRatio', aspectRatio );
+  }
+  $( this ).height( newHeight );
+  $( this ).width( parseInt(newHeight * aspectRatio, 10) );
+};
+
+/**
+ * Override .sticky() if the viewport has been scrolled past $wrapper so that
+ * the sticky element does not overlap content that comes after $wrapper.
+ * @return {null}
+ */
+function updateStickiness() {
+  var max = fx.$wrapper.offset().top + fx.$wrapper.height() - fx.$imageMapWrapper.height(),
+      stickBottom = 'image-map_image__stick-to-bottom';
+  if ( fx.$window.scrollTop() >= max && !fx.$imageMapWrapper.hasClass( stickBottom ) ) {
+    fx.$imageMapWrapper.addClass( stickBottom );
+  } else if ( fx.$window.scrollTop() < max && fx.$imageMapWrapper.hasClass( stickBottom ) ) {
+    fx.$imageMapWrapper.removeClass( stickBottom );
+  }
+}
+
+/**
+ * Initialize the form explainer app.
+ * @return {null}
+ */
+function init() {
+  $(document).ready(function(){
+    setDimensions();
+    fx.$imageMapWrapper.sticky({ className: 'has-sticky' });
+    fx.$window.on( 'scroll', updateStickiness );
+  });
+}
 
 // Do it!
 init();
