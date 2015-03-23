b959c0806992e2a08dc94ce2996e117b218b2ab7
Catherine Farman <Catherine.Farman@cfpb.gov>
2015-03-23 17:27:07 -0400

    use cf-expandables module for secondary nav, update resize behavior

--- src/static/js/modules/secondary-nav.js
+++ src/static/js/modules/secondary-nav.js
@@ -1,10 +1,15 @@
 var $ = require('jquery');
+require('../../vendor/jquery.easing/jquery.easing.js');
+require('../../vendor/cf-expandables/cf-expandables.js');
 
 var $navBtn = $('.nav-secondary_link__button'),
-    $navContent = $('.nav-secondary .expandable-content');
+    $navContent = $('.nav-secondary .expandable_content'),
+    $nav = $('.nav-secondary.expandable');
 
 var toggleCheck = function() {
-  if ($navBtn.css('display') !== 'none' ){
+    // if button is visible, it's an expandable
+  if ($navBtn.css('display') !== 'none' && !$nav.hasClass('expandable__expanded')) {
+    // hide content if it's not expanded
     $navContent.css('display', 'none');
   } else {
     $navContent.css('display', 'block');
