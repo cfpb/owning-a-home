2c662807b13d759482bf50947ec4673789d5855e
Catherine Farman <Catherine.Farman@cfpb.gov>
2015-04-07 15:02:39 -0400

    update click_link method to fix element is not clickable error

--- test/browser_testing/features/pages/navigation.py
+++ test/browser_testing/features/pages/navigation.py
@@ -29,7 +29,8 @@ class Navigation(Base):
         # observed with SauceLabs screencast
         script = "arguments[0].scrollIntoView(true);"
         self.driver.execute_script(script, element)
-        element.click()
+        action = webdriver.ActionChains(self.driver).move_to_element_with_offset(element, 0, 20).click()
+        action.perform()
 
     def click_learn_more_link(self, section_name):
         xpath = "//h3[text()='"+ section_name + "']/../p/a/span[text()='Learn more']"
@@ -39,4 +40,4 @@ class Navigation(Base):
         # observed with SauceLabs screencast
         script = "arguments[0].scrollIntoView(true);"
         self.driver.execute_script(script, element)
-        element.click()
+        element.click()        
