33d303bf5a2bbc0ca0dbace2540f94e203ac29fd
S Gorobet <Serghei.Gorobet@cfpb.gov>
2015-06-01 14:31:50 -0400

    modify sheer processor

--- src/_lib/wordpress_journey_processor.py
+++ src/_lib/wordpress_journey_processor.py
@@ -6,14 +6,14 @@ import dateutil.parser
 
 
 def posts_at_url(url):
-    
+
     current_page = 1
     max_page = sys.maxint
 
     while current_page <= max_page:
         url = os.path.expandvars(url)
-        resp = requests.get(url, params={'page':current_page, 'count': '-1'})
-        results = json.loads(resp.content) 
+        resp = requests.get(url, params={'page': current_page, 'count': '-1'})
+        results = json.loads(resp.content)
         current_page += 1
         max_page = results['pages']
         for p in results['posts']:
@@ -32,15 +32,14 @@ def process_journey(item):
     del item['date']
     custom_fields = item['custom_fields']
     item['_id'] = item['slug']
-    
+
     if item['parent'] != 0:
         # This is a step item
         item['has_parent'] = True
-        if custom_fields.get('what_to_know'):
-            item['what_to_know'] = custom_fields['what_to_know'][0]
-        if custom_fields.get('how_to_take_action'):
-            item['how_to_take_action'] = \
-                custom_fields['how_to_take_action'][0]
+        item_fields = ['what_to_know', 'how_to_take_action', 'pitfalls_to_avoid']
+        for field_name in item_fields:
+            if custom_fields.get(field_name):
+                item[field_name] = custom_fields[field_name][0]
         if custom_fields.get('key_tool'):
             key_tool = {}
             key_tool['url'] = custom_fields['key_tool'][0]
@@ -49,11 +48,11 @@ def process_journey(item):
     else:
         # This is a phase item
         item['has_parent'] = False
-        
+
         # create list of tools
         item['tools'] = []
-        for x in xrange(0,2):
-            tool = {}        
+        for x in xrange(0, 2):
+            tool = {}
             fields = ['description', 'link']
             for field in fields:
                 field_name = 'tools_%s_%s' % (str(x), field)
@@ -66,15 +65,15 @@ def process_journey(item):
 
             if tool:
                 item['tools'].append(tool)
-                
+
         # create list of milestones
         milestones = []
-        for x in xrange(0,3):
+        for x in xrange(0, 3):
             key = 'milestones_%s_milestone' % x
             if key in custom_fields:
                 milestones.append(custom_fields[key][0])
 
-        if milestones:     
+        if milestones:
             item['milestones'] = milestones
 
     return item
