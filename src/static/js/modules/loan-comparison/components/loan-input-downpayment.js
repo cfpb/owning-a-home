fe3936fd015dffa0fe4a0f5ea087e8cf97b69f25
Virginia Czosek <Virginia.Czosek@cfpb.gov>
2015-06-04 12:30:19 -0400

    fix(lc): make input table appear flush left on desktop
    Also moves downpayment-percent calculation back out to loan store,
    since this value is displayed in the mobile loan summary section,
    and fixes the closing costs concatenation instead of addition issue.

--- src/static/js/modules/loan-comparison/components/loan-input-downpayment.js
+++ src/static/js/modules/loan-comparison/components/loan-input-downpayment.js
@@ -5,25 +5,6 @@ var assign = require('object-assign');
 var mortgageCalculations = require('../mortgage-calculations');
 
 var LoanDownpaymentInput = React.createClass({
-    getInitialState: function () {
-        return this.getDownpaymentState(this.props.loan);
-    },
-    componentWillReceiveProps: function (nextProps) {
-        this.setState(this.getDownpaymentState(nextProps.loan));
-    },
-    getDownpaymentState: function (loan) {
-        return {
-            'downpayment': loan['downpayment'],
-            'downpayment-percent': mortgageCalculations['downpayment-percent'](loan)
-        };
-    },
-    updateDownpayment: function (percent) {
-        this.setState({
-            'downpayment-percent': percent,
-            'downpayment': mortgageCalculations['downpayment'](assign({}, this.props.loan, {'downpayment-percent': percent}))
-        });
-        this.props.onChange(this.state.downpayment);
-    },
     // TODO: move error display to table-row?
     showError: function() {
         var loan = this.props.loan;
@@ -38,16 +19,16 @@ var LoanDownpaymentInput = React.createClass({
         return (
             <div>
                 <TextInput
-                    value={this.state['downpayment-percent']}
+                    value={this.props.loan['downpayment-percent']}
                     className='small-input percent-input' 
                     maxLength='2' 
                     placeholder='10' 
-                    onChange={this.updateDownpayment}/>
+                    onChange={this.props.onChange.bind(this, 'downpayment-percent')}/>
                 <TextInput 
-                    value={this.state['downpayment']}
+                    value={this.props.loan['downpayment']}
                     className='mid-input dollar-input' 
                     placeholder='20,000' 
-                    onChange={this.props.onChange}/>
+                    onChange={this.props.onChange.bind(this, 'downpayment')}/>
                 <ErrorMessage opts={{showMessage: this.showError}}/>
             </div>
         );
