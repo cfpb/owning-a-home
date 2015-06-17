955ab6b70388ec060f31e2e89ddd3487a86571bf
Virginia Czosek <Virginia.Czosek@cfpb.gov>
2015-06-17 17:54:11 -0400

    fix(lc): require loan store & jquery in before block

--- test/js/loan_store_test.js
+++ test/js/loan_store_test.js
@@ -1,29 +1,33 @@
 var chai = require('chai');
 var expect = chai.expect;
-var loanStore = require('../../src/static/js/modules/loan-comparison/stores/loan-store.js');
-var  $ = require('jquery');
+var loanStore;
+var $;
 
 
 
 describe('Loan store validation', function() {
- 
-  describe('loan type validation', function() {
-    
-    it('should return an error for a VA ARM loan', function() {
-      expect(loanStore.validators['loan-type']({'rate-structure': 'arm', 'loan-type': 'va'})).to.exist()
+    before(function () {
+        $ = require('jquery');
+        loanStore = require('../../src/static/js/modules/loan-comparison/stores/loan-store.js');
     });
     
-    it('should change FHA ARM loan to conventional', function() {
-      var loan = {'rate-structure': 'arm', 'loan-type': 'fha'};
-      loanStore.validators['loan-type'](loan);
-      expect(loan['loan-type']).to.equal('conf')
+    describe('loan type validation', function() {
+
+        it('should return an error for a VA ARM loan', function() {
+            expect(loanStore.validators['loan-type']({'rate-structure': 'arm', 'loan-type': 'va'})).to.exist()
+        });
+
+        it('should change FHA ARM loan to conventional', function() {
+            var loan = {'rate-structure': 'arm', 'loan-type': 'fha'};
+            loanStore.validators['loan-type'](loan);
+            expect(loan['loan-type']).to.equal('conf')
+        });
+
+        it('should not return an error for a conventional ARM loan', function() {
+            expect(loanStore.validators['loan-type']({'rate-structure': 'arm', 'loan-type': 'conf'})).not.to.exist();
+        });
+
     });
-    
-    it('should not return an error for a conventional ARM loan', function() {
-      expect(loanStore.validators['loan-type']({'rate-structure': 'arm', 'loan-type': 'conf'})).not.to.exist();
-    });
-    
-  });
 
 
 });
