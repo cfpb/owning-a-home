8ed2962a71e5a30845de85fc08f84520e9bdbcd8
Virginia Czosek <Virginia.Czosek@cfpb.gov>
2015-06-17 17:26:47 -0400

    test(lc): add prelim loan store tests

--- test/js/loan_store_test.js
+++ test/js/loan_store_test.js
@@ -1,1 +1,30 @@
+var chai = require('chai');
+var expect = chai.expect;
+var loanStore = require('../../src/static/js/modules/loan-comparison/stores/loan-store.js');
+var  $ = require('jquery');
+
+
+
+describe('Loan store validation', function() {
+ 
+  describe('loan type validation', function() {
+    
+    it('should return an error for a VA ARM loan', function() {
+      expect(loanStore.validators['loan-type']({'rate-structure': 'arm', 'loan-type': 'va'})).to.exist()
+    });
+    
+    it('should change FHA ARM loan to conventional', function() {
+      var loan = {'rate-structure': 'arm', 'loan-type': 'fha'};
+      loanStore.validators['loan-type'](loan);
+      expect(loan['loan-type']).to.equal('conf')
+    });
+    
+    it('should not return an error for a conventional ARM loan', function() {
+      expect(loanStore.validators['loan-type']({'rate-structure': 'arm', 'loan-type': 'conf'})).not.to.exist();
+    });
+    
+  });
+
+
+});
 
