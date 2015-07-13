ba4a1f717e88bcf7435a5e29aac0f05abcc6a3f7
Virginia Czosek <Virginia.Czosek@cfpb.gov>
2015-07-13 15:56:25 -0400

    feat(lc): add print button functionality
    Also make React work in IE with polyfills

--- src/static/js/modules/loan-comparison.js
+++ src/static/js/modules/loan-comparison.js
@@ -4,7 +4,6 @@ require('cf-expandables');
 var React = require('react');
 var App = require('./loan-comparison/components/app');
 
-
 React.render(
   <App/>, document.getElementById('app-container')
 );
