7a78c2aaa5f2c4db124b9e15bacefb75bcd4eff9
S Gorobet <Serghei.Gorobet@cfpb.gov>
2015-08-24 10:59:53 -0400

    address a couple of TODOs in tests

--- test/js/loan-comparison-api_test.js
+++ test/js/loan-comparison-api_test.js
@@ -2,18 +2,26 @@ var chai = require('chai');
 var expect = chai.expect;
 var sinon = require('sinon');
 
-var api;
+var api, server, $, config, spy;
 
 describe('Loan comparison API tests', function() {
   before(function() {
     api = require('../../src/static/js/modules/loan-comparison/api.js');
+    config = require('../../config/config.json');
+    $ = require('jquery');
+  });
+
+  afterEach(function() {
+    spy.restore();
   });
 
   describe('Fetch county data', function() {
     it('should fetch county data', function() {
       var arg = {state: 'DC'};
-      //api.fetchCountyData(arg);
-      // TODO: don't know yet how to spy/stub jQuery
+      spy = sinon.spy($, 'get');
+      api.fetchCountyData(arg);
+      sinon.assert.calledOnce(spy);
+      sinon.assert.calledWith(spy, config.countyAPI, arg);
     });
   });
 
@@ -29,13 +37,27 @@ describe('Loan comparison API tests', function() {
         'credit-score': 750,
         'points': 0,
         'state': 'DC'
+      },
+      expected_data = {
+        price: 200000,
+        loan_amount: 100000,
+        minfico: 750,
+        maxfico: 769,
+        rate_structure: 'Fixed',
+        loan_term: 30,
+        loan_type: 'Type',
+        arm_type: null,
+        state: 'DC',
+        points: 0
       };
-      //api.fetchRateData(loan);
-      // TODO: what and how do I test anything here?
+      spy = sinon.spy($, 'ajax');
+      api.fetchRateData(loan);
+      sinon.assert.calledOnce(spy);
+      sinon.assert.calledWithMatch(spy, {url: config.rateCheckerAPI, data: expected_data});
     });
 
-    it('should fetch rate data, v2', function() {
-       var loan = {
+    it('should fetch rate data, credit score undefined', function() {
+      var loan = {
         price: 200000,
         'loan-amount': 100000,
         'rate-structure': 'Fixed',
@@ -45,13 +67,27 @@ describe('Loan comparison API tests', function() {
         'credit-score': null,
         'points': 0,
         'state': 'DC'
+      },
+      expected_data = {
+        price: 200000,
+        loan_amount: 100000,
+        minfico: 0,
+        maxfico: 19,
+        rate_structure: 'Fixed',
+        loan_term: 30,
+        loan_type: 'Type',
+        arm_type: null,
+        state: 'DC',
+        points: 0
       };
-      //api.fetchRateData(loan);
-      // TODO: what and how do I test anything here?
+      spy = sinon.spy($, 'ajax');
+      api.fetchRateData(loan);
+      sinon.assert.calledOnce(spy);
+      sinon.assert.calledWithMatch(spy, {url: config.rateCheckerAPI, data: expected_data});
     });
 
-    it('should fetch rate data, v3', function() {
-       var loan = {
+    it('should fetch rate data, credit score = 840', function() {
+      var loan = {
         price: 200000,
         'loan-amount': 100000,
         'rate-structure': 'Fixed',
@@ -61,17 +97,38 @@ describe('Loan comparison API tests', function() {
         'credit-score': 840,
         'points': 0,
         'state': 'DC'
+      },
+      expected_data = {
+        price: 200000,
+        loan_amount: 100000,
+        minfico: 840,
+        maxfico: 850,
+        rate_structure: 'Fixed',
+        loan_term: 30,
+        loan_type: 'Type',
+        arm_type: null,
+        state: 'DC',
+        points: 0
       };
-      //api.fetchRateData(loan);
-      // TODO: what and how do I test anything here?
+      spy = sinon.spy($, 'ajax');
+      api.fetchRateData(loan);
+      sinon.assert.calledOnce(spy);
+      sinon.assert.calledWithMatch(spy, {url: config.rateCheckerAPI, data: expected_data});
     });
   });
 
   describe('Fetch mortgage insurance data', function() {
     it('should fetch mortgage insurance data', function() {
       var loan = {param: 'value'};
-      //api.fetchMortgageInsuranceData(loan);
-      // TODO: what do I test here?
+      var today = new Date();
+      var decache = "" + today.getDate() + today.getMonth();
+      var expected_data = {
+        decache: decache,
+      };
+      spy = sinon.spy($, 'ajax');
+      api.fetchMortgageInsuranceData(loan);
+      sinon.assert.calledOnce(spy);
+      sinon.assert.calledWithMatch(spy, {url: config.mortgageInsuranceAPI});
     });
   });
 
