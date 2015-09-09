f87ee0566a3ddb0d7a3eb2d44eff4c337410ce17
Virginia Czosek <Virginia.Czosek@cfpb.gov>
2015-09-09 11:17:23 -0700

    fix(mpw): show tooltip on all monthly payment worksheet links

--- src/static/js/modules/base.js
+++ src/static/js/modules/base.js
@@ -1,2 +1,20 @@
 require('./nemo');
 require('./nemo-shim');
+
+var $ = jQuery = require('jquery');
+require('tooltips');
+
+var monthlyPaymentMsg = 'Make sure to download this PDF and open it in Adobe Acrobat.\nOtherwise, the calculations may not work correctly.';
+
+$(document).ready( function() {
+  
+  $(document).tooltip({
+      selector: 'a[href$="monthly_payment_worksheet.pdf"]',
+      'placement': 'bottom',
+      container: 'body',
+      title: function getTooltipTitle(){
+          return monthlyPaymentMsg;
+      }
+  });
+  
+});
