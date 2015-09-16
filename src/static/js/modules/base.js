fad8af8ce7b9787fdffb3024a50496c94606f713
Virginia Czosek <Virginia.Czosek@cfpb.gov>
2015-09-16 08:17:04 -0700

    fix(mpw): remove monthly payment worksheet tooltips

--- src/static/js/modules/base.js
+++ src/static/js/modules/base.js
@@ -1,19 +1,3 @@
 require('./nemo');
 require('./nemo-shim');
 
-var $ = jQuery = require('jquery');
-require('tooltips');
-
-var monthlyPaymentMsg = 'Make sure to download this PDF and open it in Adobe Acrobat.\nOtherwise, the calculations may not work correctly.';
-
-$(document).ready( function() {
-  
-  $('a[href$="monthly_payment_worksheet.pdf"]').tooltip({
-      'placement': 'bottom',
-      container: 'body',
-      title: function getTooltipTitle(){
-          return monthlyPaymentMsg;
-      }
-  });
-  
-});
